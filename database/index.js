6b77b454ca84f27f41aff82add3898587d35e5eb
Eric Mai <maieric25@gmail.com>
2018-12-20 15:24:57 -0800

    Schema created ready to be run

--- database/index.js
+++ database/index.js
@@ -1,19 +1,23 @@
-const mongoose =  require('mongoose');
+const mongoose = require ('mongoose');
 mongoose.connect('mongodb://localhost/houses', {} , (err)=>{
-    if(err){
-        console.log(err)
-    }
-})
+  if (err) {
+    console.log(err);
+  }
+  console.log('im connected?');
+});
 
 let repoSchema = mongoose.Schema({
-    _id : {type:Number, unique: true, index:true},
-    postTitle: Text,
-    cost: Number,
-    image: Text,
-    roomtype:{type: Text , beds: Number, Premium: Boolean},
-    rating : {StarNum : Number, Votes : Numbers},
-    description: Text
-})
+  _id: {type: Number, unique: true, index: true},
+  postTitle: Text,
+  cost: Number,
+  image: Text,
+  roomtype: {type: Text, beds: Number, Premium: Boolean},
+  rating: {StarNum: Number, Votes: Numbers},
+  description: Text
+});
 
-let repo = mongoose.model("Similar", repoSchema);
+let repo = mongoose.model('Similar', repoSchema);
 
+module.exports = {
+  repoSchema,
+};
