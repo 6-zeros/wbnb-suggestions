f8521c2fba2988ed5298fc61ad586c042c452c69
Eric Mai <maieric25@gmail.com>
2018-12-20 12:19:26 -0800

    Connection established

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const app = express()
-const port = 3000;
+let app = express();
+let port = 3123;
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/../public'));
+
 
 
 app.listen(port, ()=>{
-    console.log('we all good')
-})
+  console.log('we all good');
+});
