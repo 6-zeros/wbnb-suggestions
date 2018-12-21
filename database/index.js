4166a6e4028128286691db1dcd8df658e3c4ceb4
Eric Mai <maieric25@gmail.com>
2018-12-20 19:06:33 -0800

    commit fixes

--- database/index.js
+++ database/index.js
@@ -1,24 +1,25 @@
- const mongoose = require ('mongoose');
- 
-mongoose.connect('mongodb://localhost/houses', {} , (err)=>{
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/houses', {}, (err) =>{
   if (err) {
     console.log(err);
   }
   console.log('im connected?');
 });
 
-let repoSchema = mongoose.Schema({
-  _id: {type: Number, unique: true, index: true},
+const photoCaraSchema = mongoose.Schema({
+  _id: { type: Number, unique: true, index: true },
   postTitle: Text,
   cost: Number,
   image: Text,
-  roomtype: {type: Text, beds: Number, Premium: Boolean},
-  rating: {StarNum: Number, Votes: Numbers},
+  roomtype: { type: Text, beds: Number, Premium: Boolean },
+  rating: { StarNum: Number, Votes: Numbers },
   description: Text
 });
 
- let Carousel = mongoose.model('Similar', repoSchema);
+const repo = mongoose.model('Similar', photoCaraSchema);
 
 module.exports = {
-  repoSchema,
+  photoCaraSchema,
+  repo,
 };
