596ebbfb1136f8cb34e77892db79087baa5af77b
seymakara <seymakara88@gmail.com>
2019-01-10 17:47:30 -0800

    cleaned the code

--- database/dbseeder.js
+++ database/dbseeder.js
@@ -1,19 +1,7 @@
 const mongoose = require("mongoose");
 const loremIpsum = require("lorem-ipsum");
 const fs = require('fs');
-// const db = require('./index.js');
 const path = require('path');
-
-// const para = loremIpsum({
-//   count: 1,
-//   units: "paragraph",
-//   sentenceLowerBound: 5,
-//   sentenceUpperBound: 15,
-//   paragraphLowerBound: 3,
-//   paragraphUpperBound: 7,
-//   format: "plain",
-//   random: Math.random
-// });
 
 const sent = loremIpsum({
   count: 1,
@@ -24,35 +12,9 @@ const sent = loremIpsum({
   random: Math.random
 });
 
-// const randomBool = Math.random() >= 0.5;
 const randomNum = (from, to) => {
   return Math.floor(Math.random() * to + 1) + from;
 };
-
-
-// const seedGenerateor = (/* entries */) => {
-
-//   var stream = fs.createWriteStream('./database/data.csv', { flags: 'w' });
-//   stream.write("id,title,cost,picture,reviewCount,stars,beds\n")
-//   // const generatedData = [];
-//   for (let i = 0; i <= 5000000; i += 1) {
-//     imgId = (i % 981) + 1
-//     let id = i + 1
-//     let title = sent
-//     // prem: randomBool
-//     let cost = randomNum(50, 200)
-//     let picture = `https://s3-us-west-1.amazonaws.com/wbnb-suggestions-images/img${imgId}.jpg`
-//     let reviewCount = randomNum(20, 1500)
-//     let stars = randomNum(1, 5)
-//     let beds = randomNum(1, 7)
-//     // favorite: randomBool
-//     // description: para
-//     var listingDetail = `${id},${title},${cost},${picture},${reviewCount},${stars},${beds}\n`
-//     stream.write(listingDetail);
-//     // generatedData.push(photo);
-//   }
-//   // return generatedData;
-// };
 
 var current = -1;
 var stream = fs.createWriteStream('./database/data.csv', { flags: 'w' });
@@ -84,8 +46,6 @@ function seedGenerator() {
 
 }
 
-
-// const generatedData = seedGenerateor(1000000);
 seedGenerator()
 
 // const seeder = () => {
@@ -104,11 +64,3 @@ seedGenerator()
 //     }
 //   });
 // };
-
-
-// fs.writeFile('./database/data.txt', JSON.stringify(generatedData), (err, data) => {
-//   if (err) {
-//     return console.log('Error in writing', err);
-//   }
-//   // seeder();
-// });
